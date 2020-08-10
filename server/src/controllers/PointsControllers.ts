262eed87ae967c816b0b5230cbe258b47d04e0f7
Lucas Aoun <souolucas2009@gmail.com>
2020-08-10 10:08:49 -0300

    finished

--- server/src/controllers/PointsControllers.ts
+++ server/src/controllers/PointsControllers.ts
@@ -53,7 +53,7 @@ class PointsController {
         const trx = await knex.transaction(); // Só inserem no banco de dados se todas elas funcionarem
     
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name, // short-syntax
             email,
             whatsapp,
